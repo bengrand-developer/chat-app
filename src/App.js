4d41fcc6a19d3de6ff4080b59077b2a92fcd8f8c
GRAND <bengrand.pro@gmail.com>
2020-03-28 08:15:13 +0100

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import './App.css';
-import Contact from './components/Contact.js'
+import Contact from './components/Contact';
 
 function App() {
+  const users = [
+    {
+      name : 'Merlin',
+      avatar: 'https://www.moyenagepassion.com/wp-content/uploads/2016/02/Merlin_enchanteur_kaamelott_serie_culte_roi_arthur_moyen-age_passion.jpg',
+      online : true
+    },
+    {
+      name : 'Kadoc',
+      avatar: 'https://vignette.wikia.nocookie.net/kaamelott-officiel/images/9/9d/Kadoc.jpg/revision/latest/top-crop/width/360/height/450?cb=20151113170204&path-prefix=fr',
+      online : false
+    },
+    {
+      name : 'Gauvain',
+      avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRrOdH8aUuB7IE_QslUMw2avRoQkIAFuLeu6KMk_a3CZDQG6eLQ',
+      online : true
+    }
+   
+  ];
+  const listUsers = users.map((user) => {
+    return <Contact {...user}/> 
+  });
+
   return (
-    <div className= 'Contact'>
-      <Contact />
-       
+    <div className="App">
+      {listUsers}
     </div>
-    
   );
 }
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,31 @@
 import  React from 'react';
+import PropTypes from 'prop-types';
 import './Contact.css';
 
 
 
-function Contact() {
-    return (
-      <div className="Contact">
-        <img className="avatar" src='https://randomuser.me/api/portraits/women/49.jpg' alt="Joann Chambers"/>
-        <div className="name">Joann Chambers
-                <div className="status">
-                    <div className="status-online"></div>
-                        <div className="status-text"><p>Online</p>
-                
-                            </div>
-                        </div>
-                    </div>
-      </div>
+function Contact(user) {
+    return(
+
+    <div className="Contact">
+        <img className="avatar" src={user.avatar} alt= {user.name}/>
+        <div>
+            <h6 className="name"> {user.name} </h6>
+            <div className="status">             
+                {user.online? <p className = 'status-online'> </p> : <p className = 'status-offline'></p>}
+                {user.online? <p className = 'status-text'>online</p> : <p className = 'status-text'>offline</p>}
+            </div>
+        </div>
+    </div>
     );
   }
+  
+  Contact.propTypes = {
+    name: PropTypes.objectOf(PropTypes.user).isRequired,
+    avatar: PropTypes.objectOf(PropTypes.user).isRequired,
+    online: PropTypes.objectOf(PropTypes.user).isRequired,
+  };
+ 
+  
 
   export default Contact;
