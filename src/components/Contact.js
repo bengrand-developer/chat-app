faba38efa69ad72672f601da2f68d065b4ef0357
GRAND <bengrand.pro@gmail.com>
2020-03-31 16:55:25 +0200

    add status button

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,27 +2,46 @@ import React from "react";
 import "./Contact.css";
 import PropTypes from 'prop-types';
 
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online : false
+    }
+  };
 
-function Contact(props) {
-  return (
-    <div className="Contact">
-      <img className="avatar" src={props.avatar} alt={props.name}/>
+  changeStatus() {
+    const newStatus = !this.state.online
+    this.setState({online: newStatus})
+    console.log("passe");
+}
+
+    render(){
+    return(
+      <div className="Contact">
+      <img className="avatar" src={this.props.avatar} alt={this.props.name}/>
       <div>
-        <h4 className="name">{props.name}</h4>
-        <div className="status">
-            {props.online ? <p className="status-online"></p> : <p className="status-offline"></p>}
-            {props.online ? <p className="status-text">online</p> : <p className="status-text">offline</p>}        
+        <h4 className="name">{this.props.name}</h4>
+        <div className="status">   
+        <div onClick={this.changeStatus.bind(this)} className={this.state.online? 'status-online' : 'status-offline'}></div>
+        <p className='status-text'>{this.state.online? 'online' : 'offline'}</p>       
         </div>
       </div>
     </div>
-  );
+      
+    )
+  }
 }
+
+
 
 Contact.propTypes = {
   name: PropTypes.objectOf(PropTypes.props).isRequired,
   avatar: PropTypes.objectOf(PropTypes.props).isRequired,
   online: PropTypes.objectOf(PropTypes.props).isRequired,
 };
+
+
 
 export default Contact;
 
